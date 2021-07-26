b85b83a45a294dde33177b8546ce9b8fd9e203c6
Willian <willian.adao@linkc.com.br>
2021-07-26 11:09:57 -0300

    Inicialização do projeto

---
+++ src/main/java/com/lilosolutions/recolhawebapi/RecolhaWebApiApplication.java
@@ -0,0 +1,14 @@
+package com.lilosolutions.recolhawebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecolhaWebApiApplication {
+
+	// Inicio
+	public static void main(String[] args) {
+		SpringApplication.run(RecolhaWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lilosolutions/recolhawebapi/RecolhaWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilosolutions.recolhawebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecolhaWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
